a4de61ff7f0df03252219ca2b820a6b4215dafe7
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2017-08-07 17:33:42 +0200

    Update WFSim description and WFObs description. Now all comments are to be added to the internal codes

--- WFObs_demo.m
+++ WFObs_demo.m
@@ -8,15 +8,44 @@ clear all; close all; clc;
 %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %  %
 %
 %%   Quick use:
-%     1. Specify your script preferences in lines 49 - 59
-%     3. Specify your simulation settings file in line  62
-%      a. To create a simulation case manually, have a look at the ./configurations folder
+%     1. Specify your script preferences in lines 58 - 73
+%     3. Specify your simulation settings file in line  76
+%      a. if you want (OPTIONAL!), you can create a simulation case 
+%         manually, then have a look at the ./configurations folder
 %          i.  Use  '/Setup_sensors/WFObs_s_sensors.m' to specify measurement locations
 %          ii. All WFSim model/site information is contained in '../WFSim/bin/core/meshing.m'
 %     3. Press start.
 %
 %%   Relevant input/output variables
-
+%     - configName: name of the simulation case that is to be simulated.
+%     All simulation scenarios can be found in the '/configurations/'
+%     folder as seperate files. The default case is 'YawCase3.m'.
+%
+%     - scriptOptions: this struct contains all simulation settings, not
+%     related to the wind farm itself (solution methodology, outputs, etc.)
+%
+%     - OutputData (*): this struct contains all interesting outputs. It packs
+%       several substructs, namely:
+%       - *.Wp: this struct contains all the simulation settings related to 
+%         the wind farm, the turbine inputs, the atmospheric properties, etc.
+%         See WFSim.m for a more elaborate description of 'Wp'.
+%
+%       - *.sol_array: this cell array contains the system states at every
+%         simulated time instant. Each cell entry contains a sol struct.
+%         See WFSim.m for a more elaborate description of 'sol'. In
+%         addition to the entries described in WFSim.m, each 'sol' struct
+%         contains in addition:
+%           *.sol.score: a struct containing estimation performance scores
+%           such as the maximum estimation error, the RMS error, and the
+%           computational cost (CPU time).
+%           *.sol.measuredData: a struct containing the true (to be
+%           estimated) values, and the measurement data fed into the
+%           estimation algorithm.
+%
+%       - *.strucObs: this struct contains all the observer settings and
+%       (temporary) files used for updates, such as covariance matrices,
+%       ensemble/sigma point sets, measurement noise, etc.
+%
 %%   Debugging and contributing:
 %     - First, try to locate any errors by turning all possible outputs
 %       on (printProgress, printConvergence, Animate, plotMesh).
@@ -31,7 +60,7 @@ scriptOptions.printConvergence  = 0;  % Print convergence parameters every times
 
 % Visualization settings
 scriptOptions.plotMesh          = 0;  % Show meshing and turbine locations
-scriptOptions.Animate           = 0;  % Show results every x iterations (0: no plots)
+scriptOptions.Animate           = 1;  % Show results every x iterations (0: no plots)
    scriptOptions.plotContour    = 1;  % Show flow fields
    scriptOptions.plotPower      = 0;  % Plot true and predicted power capture vs. time
    scriptOptions.plotError      = 0;  % plot RMS and maximum error vs. time
@@ -39,13 +68,12 @@ scriptOptions.Animate           = 0;  % Show results every x iterations (0: no p
 
 % Saving settings
 scriptOptions.savePlots         = 0;  % Save all plots in external files at each time step
-scriptOptions.saveEst           = 1;  % Save estimated flow fields & powers in an external file at each time step
-scriptOptions.saveWorkspace     = 1;  % Save complete workspace at the end of simulation
-scriptOptions.savePath          = ['results/YawCase3_enkf']; % Destination folder of saved files
+scriptOptions.saveEst           = 0;  % Save estimated flow fields & powers in an external file at each time step
+scriptOptions.saveWorkspace     = 0;  % Save complete workspace at the end of simulation
+scriptOptions.savePath          = ['results/tmp']; % Destination folder of saved files
 
-% Model and observer configuration file
-configName = 'YawCase3.m'; % configuration filename. See './configurations' for options: 'NoPrecursor', 'YawCase3'
-
+% Configuration file
+configName = 'YawCase3.m'; % See './configurations' for options: i.e., 'APC.m' or 'YawCase3.m'
 
 
 %% Execute the WFObs core code
