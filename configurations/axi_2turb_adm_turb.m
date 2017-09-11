f3f390924a6c4c333cec5386d8bf5ddf1727f879
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2017-09-11 14:49:18 +0200

    Clean up configurations -- clearly differentiate between integers, floats and booleans

--- configurations/axi_2turb_adm_turb.m
+++ configurations/axi_2turb_adm_turb.m
@@ -2,11 +2,11 @@
 Wp.name = '2turb_adm_turb';  % Name of meshing (from '/WFSim/bin/core/meshing.m')
 
 %% WFSim model settings
-scriptOptions.startUniform    = 0;    % Start from a uniform flow field (1) or from a fully developed waked flow field (0).
-scriptOptions.conv_eps        = 1e-6; % Convergence parameter
-scriptOptions.max_it_dyn      = 1;    % Convergence parameter
+scriptOptions.startUniform = false; % Start from a uniform flow field (1) or from a fully developed waked flow field (0).
+scriptOptions.conv_eps     = 1e-6;  % Convergence parameter
+scriptOptions.max_it_dyn   = 1;     % Convergence parameter
 
-if scriptOptions.startUniform==1
+if scriptOptions.startUniform
     scriptOptions.max_it = 1;   % Iteration limit for simulation start-up
 else
     scriptOptions.max_it = 50;  % Iteration limit for simulation start-up
@@ -15,9 +15,9 @@ end
 
 %% Filter settings
 % General settings
-strucObs.loadRandomSeed  = 1;      % Load a predefined random seed (for one-to-one comparisons between simulation cases)
-strucObs.noise_obs       = 0.1;    % Disturbance amplitude (m/s) in output data by randn*noiseampl ('0' for no noise)
-strucObs.noise_init      = 0.0;    % Disturbance amplitude (m/s) in initial flow field by randn*noiseinit ('0' recommended)
+strucObs.loadRandomSeed = true; % Load a predefined random seed (for one-to-one comparisons between simulation cases)
+strucObs.noise_obs      = 0.1;  % Disturbance amplitude (m/s) in output data by randn*noiseampl ('0' for no noise)
+strucObs.noise_init     = 0.0;  % Disturbance amplitude (m/s) in initial flow field by randn*noiseinit ('0' recommended)
 
 % DISABLED FOR NOW:
 % % Estimate freestream conditions
@@ -41,8 +41,8 @@ switch lower(strucObs.filtertype)
         strucObs.P_0 = 0.5; % Initial state covariance matrix
 
         % Other model settings
-        scriptOptions.exportPressures = 0; % Model/predict/filter pressure terms
-        scriptOptions.Linearversion   = 1; % Calculate linearized system matrices: necessary for ExKF
+        scriptOptions.exportPressures = false; % Model/predict/filter pressure terms
+        scriptOptions.Linearversion   = true;  % Calculate linearized system matrices: necessary for ExKF
         
         
     % Sliding mode observer (SMO)    
@@ -51,15 +51,15 @@ switch lower(strucObs.filtertype)
         strucObs.alpha = 0.01; 
         
         % Other model settings
-        scriptOptions.exportPressures = 0; % Model/predict/filter pressure terms
-        scriptOptions.Linearversion   = 1; % Calculate linearized system matrices: necessary for SMO
+        scriptOptions.exportPressures = false; % Model/predict/filter pressure terms
+        scriptOptions.Linearversion   = true;  % Calculate linearized system matrices: necessary for SMO
         
         
     % Unscented Kalman filter (UKF)
     case {'ukf'}
         % General settings
         strucObs.stateEst             = false;  % Do state estimation: true/false
-        scriptOptions.exportPressures = 0;  % Model, predict and filter pressure terms
+        scriptOptions.exportPressures = false;  % Model, predict and filter pressure terms
         
         % Covariances
         strucObs.R_k   = 0.10;  % Measurement   covariance matrix
@@ -82,18 +82,17 @@ switch lower(strucObs.filtertype)
         % Sigma-point generation settings
         strucObs.alpha = 1e0;
         strucObs.beta  = 2; % 2 is optimal for Gaussian distributions
-        strucObs.kappa = 0;% "0" or "3-L"
+        strucObs.kappa = 0; % "0" or "3-L"
 
         % Other model settings
-        scriptOptions.Linearversion   = 0;   % Calculate linearized system matrices
+        scriptOptions.Linearversion = false;   % Calculate linearized system matrices
         
 
     % Ensemble Kalman filter (EnKF)    
     case {'enkf'}
         % General settings
-        strucObs.nrens      = 50; % Ensemble size
-        strucObs.resampling = 0;  % Redistribute particles every timestep. false = classical EnKF.
-        scriptOptions.exportPressures = 0; % Include pressure terms in ensemble members (default: false)
+        strucObs.nrens = 50; % Ensemble size
+        scriptOptions.exportPressures = false; % Include pressure terms in ensemble members (default: false)
         
         % Model state covariances
         strucObs.stateEst = true;  % Estimate model states
@@ -107,9 +106,9 @@ switch lower(strucObs.filtertype)
         strucObs.W_0.p    = 0.00;  % Only used for case Projection = 0
         
         % Inflation and localization
-        strucObs.r_infl         = 1.025;  % Covariance inflation factor (typically 1.00-1.20, no inflation: 1)
-        strucObs.f_locl         = 'gaspari'; % Localization method: 'off', 'gaspari' (Gaspari-Cohn 1999) or 'heaviside' (Heaviside step function: 0s or 1s)
-        strucObs.l_locl         = 131;    % Gaspari-Cohn: typically sqrt(10/3)*L with L the cut-off length. Heaviside: cut-off length (m).
+        strucObs.r_infl = 1.025;  % Covariance inflation factor (typically 1.00-1.20, no inflation: 1)
+        strucObs.f_locl = 'gaspari'; % Localization method: 'off', 'gaspari' (Gaspari-Cohn 1999) or 'heaviside' (Heaviside step function: 0s or 1s)
+        strucObs.l_locl = 131.;    % Gaspari-Cohn: typically sqrt(10/3)*L with L the cut-off length. Heaviside: cut-off length (m).
         
         % Parameter estimation settings
         strucObs.tune.est  = false; % Estimate model parameters
@@ -120,13 +119,13 @@ switch lower(strucObs.filtertype)
         strucObs.tune.ub   = [15.00]; %[3.00,2.50]; % Upper bounds
 
         % Other settings
-        scriptOptions.Linearversion   = 0; % Disable unnecessary calculations in model
+        scriptOptions.Linearversion = false; % Disable unnecessary calculations in model
         
 
     % No filter, just open-loop simulation
     case {'sim'}
-        scriptOptions.exportPressures = 1; % Must be 'true' for sim case.
-        scriptOptions.Linearversion   = 0; % Calculate linearized system matrices
+        scriptOptions.exportPressures = true;  % Must be 'true' for sim case.
+        scriptOptions.Linearversion   = false; % Calculate linearized system matrices
         strucObs.measFlow             = false; % Use flow measurements (LIDAR) in estimates
         strucObs.measPw               = false; % Use power measurements (SCADA) from turbines in estimates
         
